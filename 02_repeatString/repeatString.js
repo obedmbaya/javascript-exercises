39616870d2e4cc287c31e9b2a55ca95757d9f855
obedmbaya <obed.mbaya@yahoo.com>
2025-01-04 12:30:44 +0200

    all test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let word = "";
+    for (let i = 0; i < num; i++) {
+        word += string;
+    }
 
+
+    if (num < 0) {
+        word = "ERROR";
+    }
+    return word;
 };
 
 // Do not edit below this line
