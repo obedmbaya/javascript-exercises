aa6e5ce5dc801f600d260d52de970a1ad6689d60
obedmbaya <obed.mbaya@yahoo.com>
2025-01-04 14:13:56 +0200

    my solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if ((typeof first !== "number" || typeof second !== "number")) {
+        return "ERROR";
+    }
+
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (first < second) {
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+    }
+
+    else {
+        for (let i = second; i <= first; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 
 };
 
