bad0e2fc55d53ea900b5b0c371f20c0824a25d7a
obedmbaya <obed.mbaya@yahoo.com>
2025-01-04 13:57:19 +0200

    splice in reverse from removing in array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...Args) {
+    let arr = array;
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = arr.length - 1; j >= 0; j--) {
+            if (arguments[i] === arr[j]) {
+                arr.splice(j,1);
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
