888720775fc6f516e8773c153841af82b4b2ebdc
obedmbaya <obed.mbaya@yahoo.com>
2025-01-04 18:37:38 +0200

    celsius and farenheit conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsiusCalculate = (farenheit - 32) * 5/9;
+  let celsiusRounded = Math.round(celsiusCalculate * 10) /10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheitCalculate = (celsius * (9/5)) + 32;
+  let farenheitRounded = Math.round(farenheitCalculate * 10) / 10;
+  return farenheitRounded;
 };
 
 // Do not edit below this line
